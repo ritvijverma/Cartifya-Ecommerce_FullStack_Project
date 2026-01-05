584f23009fc24f00ef829db32c84879a2741cc96
RITVIJ VERMA <104303884+mysteriousrv@users.noreply.github.com>
2026-01-05 16:01:42 +0530

    Swagger Doc Setup & Register Api Doc is done

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,6 +6,56 @@ import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 //routing object
 const router = express.Router()
 
+//swagger api documentation
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *         - phone
+ *         - address
+ *         - answer
+ *       properties:
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *         phone:
+ *           type: string
+ *         address:
+ *           type: string
+ *         answer:
+ *           type: string
+ */
+
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User registered successfully
+ *       500:
+ *         description: Error in registration
+ */
+
+
 //routing
 //REGISTER || METHOD POST
 router.post('/register',registerController)
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+//API Documentation
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "swagger-jsdoc";
+
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -7,13 +11,31 @@ import { fileURLToPath } from "url";
 
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import categogryRoute from "./routes/categoryRoute.js"
-import productRoute from "./routes/productRoute.js"
+import categogryRoute from "./routes/categoryRoute.js";
+import productRoute from "./routes/productRoute.js";
 // config env
 dotenv.config();
 
 // db connect
 connectDB();
+
+//swagger api config
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Cartifya E-commerce Full Stack Application",
+      description: "Node Express Full Stack Application",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const spec = swaggerDoc(options);
 
 const app = express();
 
@@ -30,18 +52,19 @@ const __dirname = path.dirname(__filename);
 app.use("/api/v1/auth", authRoutes);
 
 //Category routes
-app.use("/api/v1/category",categogryRoute)
+app.use("/api/v1/category", categogryRoute);
 
 //Product Route
-app.use("/api/v1/product",productRoute)
+app.use("/api/v1/product", productRoute);
 
+//swagger home route
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 // React build serve
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
